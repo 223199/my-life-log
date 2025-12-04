a597fb5459d0a7ca6c0580fb536f99f8efb7658c
223199 <162702161+223199@users.noreply.github.com>
2025-12-04 15:23:24 +0900

    Add Card and CardContent components

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = React.HTMLAttributes<HTMLDivElement>
+
+export const Card: React.FC<Props> = ({ className = '', ...rest }) => (
+  <div className={`rounded-xl border bg-white ${className}`} {...rest} />
+)
+
+export const CardContent: React.FC<Props> = ({ className = '', ...rest }) => (
+  <div className={`p-4 ${className}`} {...rest} />
+)
